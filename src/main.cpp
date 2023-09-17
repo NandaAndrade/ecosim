fce7d68c036dd8a310c474c400f7b42006cab446
Nanda Andrade <48488203+NandaAndrade@users.noreply.github.com>
2023-09-17 16:53:02 +0000

    Distribuindo as entidades

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "json.hpp"
 #include <random>
 
+using namespace std;
+
 static const uint32_t NUM_ROWS = 15;
 
 // Constants
@@ -13,6 +15,8 @@ const uint32_t HERBIVORE_MAXIMUM_AGE = 50;
 const uint32_t CARNIVORE_MAXIMUM_AGE = 80;
 const uint32_t MAXIMUM_ENERGY = 200;
 const uint32_t THRESHOLD_ENERGY_FOR_REPRODUCTION = 20;
+const uint32_t HERBIVORE_START_ENERGY = 100;
+const uint32_t CARNIVORE_START_ENERGY = 100;
 
 // Probabilities
 const double PLANT_REPRODUCTION_PROBABILITY = 0.2;
@@ -26,7 +30,7 @@ const double CARNIVORE_EAT_PROBABILITY = 1.0;
 // Type definitions
 enum entity_type_t
 {
-    empty,
+    emptyy,
     plant,
     herbivore,
     carnivore
@@ -47,7 +51,7 @@ struct entity_t
 
 // Auxiliary code to convert the entity_type_t enum to a string
 NLOHMANN_JSON_SERIALIZE_ENUM(entity_type_t, {
-                                                {empty, " "},
+                                                {emptyy, " "},
                                                 {plant, "P"},
                                                 {herbivore, "H"},
                                                 {carnivore, "C"},
@@ -64,6 +68,60 @@ namespace nlohmann
 
 // Grid that contains the entities
 static std::vector<std::vector<entity_t>> entity_grid;
+mutex mtx;
+
+void novaPlanta(pos_t posicao_planta){
+}
+
+void novoHerbivoro(pos_t posicao_herbivoro){
+}
+
+void novoCarnivoro(pos_t posicao_carnivoro){
+}
+
+bool criarVida(entity_type_t tipo){
+    mtx.lock();
+
+    if(entity_grid.size() == NUM_ROWS * NUM_ROWS){
+        mtx.unlock();
+        return false;
+    }
+
+    struct pos_t pos;
+    bool alocado = false;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    while(alocado == false){
+        std::uniform_int_distribution<> dis(0, NUM_ROWS - 1);
+        pos.i = dis(gen);
+        pos.j = dis(gen);
+
+
+        if(entity_grid[pos.i][pos.j].type == emptyy){
+            if(tipo == plant){
+                entity_grid[pos.i][pos.j] = {plant, 0, 0};
+                thread t1(novaPlanta, pos);
+                t1.detach();
+            }
+            if(tipo == herbivore){
+                entity_grid[pos.i][pos.j] = {herbivore, HERBIVORE_START_ENERGY, 0};
+                thread t1(novoHerbivoro, pos);
+                t1.detach();
+            }
+            if(tipo == carnivore){
+                entity_grid[pos.i][pos.j] = {carnivore, CARNIVORE_START_ENERGY, 0};
+                thread t1(novoCarnivoro, pos);
+                t1.detach();
+            }
+
+            alocado = true;
+        }
+    }
+    mtx.unlock();
+    return true;    
+}
+
 
 int main()
 {
@@ -94,10 +152,34 @@ int main()
 
         // Clear the entity grid
         entity_grid.clear();
-        entity_grid.assign(NUM_ROWS, std::vector<entity_t>(NUM_ROWS, { empty, 0, 0}));
+        entity_grid.assign(NUM_ROWS, std::vector<entity_t>(NUM_ROWS, { emptyy, 0, 0}));
         
         // Create the entities
         // <YOUR CODE HERE>
+
+        ////////////////////////////////////////////////////////////////////////////
+
+        int plantas = (int)request_body["plants"];
+        int herbivoros = (int)request_body["herbivores"];
+        int carnivoros = (int)request_body["carnivores"];
+
+        for(int i = 0; i < plantas; i++){
+            if(!criarVida(plant)){
+                break;
+            }
+        }
+        for(int i = 0; i < herbivoros; i++){
+            if(!criarVida(herbivore)){
+                break;
+            }
+        }
+        for(int i = 0; i < carnivoros; i++){
+            if(!criarVida(carnivore)){
+                break;
+            }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////
 
         // Return the JSON representation of the entity grid
         nlohmann::json json_grid = entity_grid; 
